229e88995a11b11e8712dd521daf03aaa8494e6a
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-16 22:25:09 -0300

    Improve main to create the application

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-import time
+from app import start
+from db import DB
 
-from fastapi import FastAPI, APIRouter
-
-from payments.api import router as payments_api
-
-
-app = FastAPI()
-router = APIRouter()
-
-@router.get("/{path:path}", status_code=404)
-def not_implement(path):
-    return {
-        'path': f"/{path}",
-        'status': 'notImplemented',
-        'now':  time.time(),
-    }
-
-app.include_router(payments_api, prefix="/api/v1")
-app.include_router(router)
+db = DB()
+app = start(db)
