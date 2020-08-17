6cded30d803d2364e7eb79c6705faf5455dc18af
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-17 19:25:32 -0300

    Implement POST /restaurant endpoint

--- payments/api.py
+++ payments/api.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-from payments.views import Healthcheck
+from payments.views import Healthcheck, Restaurant
+from payments.schemas import RestaurantSchema
+from db import db_connection, async_session
+
 
 router = APIRouter()
 
@@ -9,6 +13,23 @@ router = APIRouter()
 def healthcheck():
     return Healthcheck.status()
 
-@router.get("/transacao")
+
+@router.post("/restaurant", response_model=RestaurantSchema, status_code=201)
+def restaurante(restaurant: RestaurantSchema, db: Session = Depends(async_session)):
+    try:
+        return Restaurant.create(restaurant, db)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e),
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail="Error to create a new restaurant.",
+        )
+
+
+@router.get("/transacao", response_model=RestaurantSchema)
 def transacao():
     pass
