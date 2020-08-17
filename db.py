9ba757b251d2528f9936bab4372801aca626721c
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-17 07:39:39 -0300

    Refactor db.py

--- db.py
+++ db.py
@@ -1,13 +1,11 @@
-import logging
-
+from typing import Any, Generator
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
 
 from config import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
-
-log = logging.getLogger(__name__)
-BaseSQLAlchemy = declarative_base()
 
 
 class DB:
@@ -19,35 +17,36 @@ class DB:
                                                                   DB_NAME)
         self.engine = create_engine(self.connection_string)
 
-    def create_tables(self):
-        BaseSQLAlchemy.metadata.create_all(self.engine)
-
     def session(self):
-        """
-        Creates a new SessionContext - not a SQLAlchemy Session.
+        session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+        return session()
 
-        To get the SQLAlchemy session the session context should be used with a context_manager
-        or access the SessionContext.session object
-        """
-        return SessionContext(sessionmaker(self.engine))
+    def test_connection(self):
+        session = self.session()
+        session.execute("SELECT 1")
+        session.close()
+
+    def create_tables(self):
+        return BaseSQLAlchemy.metadata.create_all(self.engine)
 
 
-class SessionContext:
-    """
-    This class is a simple context manager around to session to take care of commit/rollback
-    pending operations.
-    """
-    def __init__(self, session):
-        self.session = session()
+db_connection = DB()
 
-    def __enter__(self):
-        log.info("Entering session context")
-        return self.session
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        if not traceback:
-            log.info("Committing db session pending operations")
-            self.session.commit()
-        else:
-            log.error("Error during db session, rolling back pending operations")
-            self.session.rollback()
+@as_declarative()
+class BaseSQLAlchemy:
+    id: Any
+    __name__: str
+
+    # Generate __tablename__ automatically
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+
+def async_session():
+    try:
+        session = db_connection.session()
+        yield session
+    finally:
+        session.close()
