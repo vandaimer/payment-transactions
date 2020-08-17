7bdef007c6a1631932cceb740bce1c9d8ab257c5
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-17 08:50:20 -0300

    Fix log main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import logging
 import app as application
 from db import db_connection
 
+log = logging.getLogger(__name__)
+
 def init():
     try:
         db_connection.test_connection()
         db_connection.create_tables()
     except Exception as e:
-        logging.error(e)
+        log.error(e)
         raise e
 
     return application.start()
