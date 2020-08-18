e28078f113198ca79dc2907afca18a411b3ace20
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-17 22:13:36 -0300

    Implement is_valid_cnpj method

--- payments/views/transaction.py
+++ payments/views/transaction.py
@@ -1,9 +1,12 @@
+import re
+
 from payments.models import Transaction as TransactionModel
 from payments.models import Restaurant as RestaurantModel
 from payments.schemas import ListOfTransactionSchema, ReturnNewTransactionSchema
 
 
 class Transaction:
+    CNPJ_REGEX = '^(\d{2})\.?(\d{3})\.?(\d{3})\/?(\d{4})\-?(\d{2})$'
 
     @staticmethod
     def all(cnpj, db):
@@ -21,3 +24,7 @@ class Transaction:
         db.commit()
 
         return ReturnNewTransactionSchema(aceito=True)
+
+    @staticmethod
+    def is_valid_cnpj(cnpj):
+        return re.search(Transaction.CNPJ_REGEX, cnpj)
