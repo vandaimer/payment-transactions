1e3d2c609d6650f09f532625be105bec7dec0307
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-17 21:02:14 -0300

    Implement GET /transacoes/estabelecimento endpoint

--- payments/api.py
+++ payments/api.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
+from pydantic import PositiveInt
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
 
-from payments.views import Healthcheck, Restaurant
-from payments.schemas import RestaurantSchema
+from payments.views import Healthcheck, Restaurant, Transaction
+from payments.schemas import RestaurantSchema, ListOfTransactionSchema
+
 from db import db_connection, async_session
 
 
@@ -30,6 +33,17 @@ def restaurante(restaurant: RestaurantSchema, db: Session = Depends(async_sessio
         )
 
 
-@router.get("/transacao", response_model=RestaurantSchema)
-def transacao():
-    pass
+@router.get("/transacoes/estabelecimento", response_model=ListOfTransactionSchema, tags=["Transaction"])
+def transacoes(cnpj: int, db: Session = Depends(async_session)):
+    try:
+        return Transaction.all(cnpj, db)
+    except NoResultFound as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Restaurant not found with cnpj: {cnpj}",
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Error to retrieve transactions with cnpj: {cnpj}",
+        )
