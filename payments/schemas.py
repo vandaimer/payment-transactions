be799e5b1d7584947bd2c50a083232ff4b2870c1
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-17 21:01:04 -0300

    Implement ListOfTransactionSchema schema

--- payments/schemas.py
+++ payments/schemas.py
@@ -1,7 +1,14 @@
-from .models import Restaurant, Transaction
+from typing import List
 from pydantic_sqlalchemy import sqlalchemy_to_pydantic
+from pydantic import BaseModel
+
+from .models import Restaurant, Transaction
 
 
 RestaurantSchema = sqlalchemy_to_pydantic(Restaurant, exclude={"id"})
 
 TransactionSchema = sqlalchemy_to_pydantic(Transaction, exclude={"id"})
+
+class ListOfTransactionSchema(BaseModel):
+    estabelecimento: RestaurantSchema = None
+    recebimentos: List[TransactionSchema] = []
