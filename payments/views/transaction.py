80cde49fb6cb747fd5695cabdcf62530d806de46
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-17 21:23:29 -0300

    Implement create method on Transaction view

--- payments/views/transaction.py
+++ payments/views/transaction.py
@@ -1,6 +1,6 @@
 from payments.models import Transaction as TransactionModel
 from payments.models import Restaurant as RestaurantModel
-from payments.schemas import ListOfTransactionSchema
+from payments.schemas import ListOfTransactionSchema, ReturnNewTransactionSchema
 
 
 class Transaction:
@@ -10,3 +10,14 @@ class Transaction:
         restaurant = db.query(RestaurantModel).filter(RestaurantModel.cnpj == str(cnpj)).one()
         transactions = db.query(TransactionModel).filter(TransactionModel.restaurant == restaurant.id).all()
         return ListOfTransactionSchema(estabelecimento=restaurant, recebimentos=transactions)
+
+    @staticmethod
+    def create(transaction, db):
+        restaurant = db.query(RestaurantModel).filter(RestaurantModel.cnpj == str(transaction.estabelecimento)).one()
+
+        new_transaction = TransactionModel(client=transaction.cliente, price=transaction.valor, description=transaction.descricao, restaurant=restaurant.id)
+
+        db.add(new_transaction)
+        db.commit()
+
+        return ReturnNewTransactionSchema(aceito=True)
