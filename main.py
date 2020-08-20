c7703995374972257f2ce2572bc95f7dc037a394
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-19 21:57:44 -0300

    Add log on main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import logging
 import app as application
 from db import db_connection
 
-log = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
+                    datefmt='%Y-%m-%dT%H:%M:%S')
+logger = logging.getLogger(__name__)
 
 
 def init():
@@ -11,9 +15,10 @@ def init():
         db_connection.test_connection()
         db_connection.create_tables()
     except Exception as e:
-        log.error(e)
+        logger.error(e)
         raise e
 
+    logger.info("Application initialized")
     return application.start()
 
 
